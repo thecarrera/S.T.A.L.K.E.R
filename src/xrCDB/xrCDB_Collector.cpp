tamlin-mike/xray-16

Compile-time refactoring. A few Lock turned into Lock*, to reduce compile time dependencies (many remain). A bunch of new headers added to split dependencies up. A bunch of compiler warnings removed by commenting out unused function/method formal argument/parameters. xrCore/_stl_extensions.h almost taken out of the equation.

src/xrCDB/xrCDB_Collector.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
 //.#include "xrCore/xrCore.h"
-#pragma hdrstop
+#pragma hdrstop // ???
 
 #include "xrCDB.h"
+#include "xrCore/_fbox.h"
 
 namespace CDB
 {
@@ -155,22 +156,23 @@ namespace CDB
 		dest.assign						(edge_count,u32(-1));
 
 		{
-			edge						*I = edges, *J;
-			edge						*E = edges + edge_count;
-			for ( ; I != E; ++I) {
-				if (I + 1 == E)
+			edge *I2 = edges, *J;
+			edge *E2 = edges + edge_count;
+			for ( ; I2 != E2; ++I2)
+			{
+				if (I2 + 1 == E2)
 					continue;
 
-				J							= I + 1;
+				J = I2 + 1;
 
-				if ((*I).vertex_id0 != (*J).vertex_id0)
+				if ((*I2).vertex_id0 != (*J).vertex_id0)
 					continue;
 
-				if ((*I).vertex_id1 != (*J).vertex_id1)
+				if ((*I2).vertex_id1 != (*J).vertex_id1)
 					continue;
 
-				dest[(*I).face_id*3 + (*I).edge_id]	= (*J).face_id;
-				dest[(*J).face_id*3 + (*J).edge_id]	= (*I).face_id;
+				dest[(*I2).face_id*3 + (*I2).edge_id]	= (*J).face_id;
+				dest[(*J).face_id*3 + (*J).edge_id]	= (*I2).face_id;
 			}
 		}
 #	if 0
@@ -281,8 +283,8 @@ namespace CDB
 	CollectorPacked::CollectorPacked(const Fbox &bb, int apx_vertices, int apx_faces)
 	{
 		// Params
-		VMscale.set		(bb.max.x-bb.min.x, bb.max.y-bb.min.y, bb.max.z-bb.min.z);
-		VMmin.set		(bb.min);
+		VMscale.set		(bb.vMax.x-bb.vMin.x, bb.vMax.y-bb.vMin.y, bb.vMax.z-bb.vMin.z);
+		VMmin.set		(bb.vMin);
 		VMeps.set		(VMscale.x/clpMX/2,VMscale.y/clpMY/2,VMscale.z/clpMZ/2);
 		VMeps.x			= (VMeps.x<EPS_L)?VMeps.x:EPS_L;
 		VMeps.y			= (VMeps.y<EPS_L)?VMeps.y:EPS_L;

thecarrera/S.T.A.L.K.E.R

src/xrCDB/xrCDB_Collector.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
 //.#include "xrCore/xrCore.h"
-#pragma hdrstop
+#pragma hdrstop // ???
 
 #include "xrCDB.h"
+#include "xrCore/_fbox.h"
 
 namespace CDB
 {
@@ -156,23 +157,23 @@ void Collector::calc_adjacency(xr_vector<u32>& dest)
     dest.assign(edge_count, u32(-1));
 
     {
-        edge *I = edges, *J;
-        edge* E = edges + edge_count;
-        for (; I != E; ++I)
+        edge* I2 = edges, * J;
+        edge* E2 = edges + edge_count;
+        for (; I2 != E2; ++I2)
         {
-            if (I + 1 == E)
+            if (I2 + 1 == E2)
                 continue;
 
-            J = I + 1;
+            J = I2 + 1;
 
-            if ((*I).vertex_id0 != (*J).vertex_id0)
+            if ((*I2).vertex_id0 != (*J).vertex_id0)
                 continue;
 
-            if ((*I).vertex_id1 != (*J).vertex_id1)
+            if ((*I2).vertex_id1 != (*J).vertex_id1)
                 continue;
 
-            dest[(*I).face_id * 3 + (*I).edge_id] = (*J).face_id;
-            dest[(*J).face_id * 3 + (*J).edge_id] = (*I).face_id;
+            dest[(*I2).face_id * 3 + (*I2).edge_id] = (*J).face_id;
+            dest[(*J).face_id * 3 + (*J).edge_id] = (*I2).face_id;
         }
     }
 #if 0
@@ -302,8 +303,8 @@ void Collector::remove_duplicate_T()
 CollectorPacked::CollectorPacked(const Fbox& bb, int apx_vertices, int apx_faces)
 {
     // Params
-    VMscale.set(bb.max.x - bb.min.x, bb.max.y - bb.min.y, bb.max.z - bb.min.z);
-    VMmin.set(bb.min);
+    VMscale.set(bb.vMax.x - bb.vMin.x, bb.vMax.y - bb.vMin.y, bb.vMax.z - bb.vMin.z);
+    VMmin.set(bb.vMin);
     VMeps.set(VMscale.x / clpMX / 2, VMscale.y / clpMY / 2, VMscale.z / clpMZ / 2);
     VMeps.x = (VMeps.x < EPS_L) ? VMeps.x : EPS_L;
     VMeps.y = (VMeps.y < EPS_L) ? VMeps.y : EPS_L;